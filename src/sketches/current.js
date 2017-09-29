21dd8ad6974ee33bfd3c824b2f25a7018e0c8ec3
Les Orchard <me@lmorchard.com>
2017-09-29 00:03:19 -0400

    Add some asteroids to current sketch

--- src/sketches/current.js
+++ src/sketches/current.js
@@ -1,4 +1,5 @@
 import dat from 'dat-gui';
+import Vector2D from '../lib/Vector2D';
 
 let World, installPlugins, world, plugins, gui;
 
@@ -46,19 +47,76 @@ function populateWorld () {
       rotation: -Math.PI / 2
     },
     Motion: {},
-    // Thruster: { deltaV: 1200, maxV: 500, active: false },
+    Thruster: { deltaV: 1200, maxV: 500, active: false },
     LateralThruster: {
-      deltaV: 5000,
-      maxV: 700,
+      deltaV: 4000,
+      maxV: 400,
       active: false
     },
     PlayerInputSteering: {
-      strafe: true,
-      usePointer: false,
-      useGamepad: false,
+      strafe: false,
+      usePointer: true,
+      useGamepad: true,
       radPerSec: Math.PI
     }
   });
+
+  spawnField(0, 0, 200, 50, 5, 5, 100);
+}
+
+function spawnAsteroid(x, y, width, height, dx, dy, dr, mass, health) {
+  world.insert({
+    Sprite: { name: 'asteroid', size: width},
+    Health: { max: health },
+    Collidable: {},
+    Bounce: { mass: mass },
+    Position: { x: x, y: y },
+    Motion: { dx: dx, dy: dy, drotation: dr }
+  });
+}
+
+function spawnField(centerX, centerY, radius=300,
+    MAX_ASTEROIDS=150, MAX_TRIES=5, MIN_SIZE=20, MAX_SIZE=300, MAX_GRAV=10) {
+
+  const vCenter = new Vector2D(centerY, centerX);
+  const vSpawn = new Vector2D(0, 0);
+  const vGrav = new Vector2D(0, 0);
+  const inField = [];
+
+  for (let idx = 0; idx < MAX_ASTEROIDS; idx++) {
+    for (let c = 0; c < MAX_TRIES; c++) {
+
+      const size = ((MAX_SIZE - MIN_SIZE) * Math.random()) + MIN_SIZE;
+      const rot = (Math.PI*4) * Math.random();
+      vSpawn.setValues(vCenter.x, vCenter.y - (((radius - 1) * Math.random()) + 1));
+      vSpawn.rotateAround(vCenter, rot);
+
+      let isClear = true;
+      for (let fldIdx = 0; fldIdx < inField.length; fldIdx++) {
+        const item = inField[fldIdx];
+        if (Math.abs(vSpawn.x - item.x) * 2 >= (size + item.width) * 1.025) { continue; }
+        if (Math.abs(vSpawn.y - item.y) * 2 >= (size + item.height) * 1.025) { continue; }
+        isClear = false;
+        break;
+      }
+      if (!isClear) { continue; }
+
+      inField.push({ x: vSpawn.x, y: vSpawn.y, width: size, height: size });
+
+      vGrav.setValues(0, Math.random() * MAX_GRAV);
+      vGrav.rotate(rot);
+
+      spawnAsteroid(
+        vSpawn.x, vSpawn.y,
+        size, size,
+        vGrav.x, vGrav.y,
+        (Math.PI * 0.25) * Math.random(),
+        4 * size * size,
+        4 * size * size
+      );
+
+    }
+  }
 }
 
 function buildWorld () {
